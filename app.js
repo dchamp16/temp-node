793add4fe2739b2e4b70ec15cd96a1ac0c4e6b37
Peter Ramos <86265240+dchamp16@users.noreply.github.com>
2021-08-21 20:47:40 -0600

    added something new

--- app.js
+++ app.js
@@ -13,9 +13,14 @@
 //npm init (step by step, press enter to skip)
 //npm init - (everything default)
 
+//npm i nodemon -D - to install nodemon
+// "scripts": {"start": "node app.js"}, - do it in package.json
+// sudo npm install -g <packageName> nodemon - install it in local terminal to globally work
+
 const _ = require('lodash')
 
 const items = [1, [2, [3, [4]]]]
 const newItems = _.flattenDeep(items)
 console.log(newItems);
+console.log('really');
 
